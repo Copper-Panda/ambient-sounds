9e118a35729b4673c4d4b014875fb8220c254ed4
hticy <hticy3214@gmail.com>
2025-06-14 07:53:40 +0000

    change bind by pinia

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,9 @@
 <script setup>
-	import {ref, reactive, onMounted, watch} from 'vue'
+	import {ref, onMounted} from 'vue'
+	import {useSoundStore} from '~/stores/global'
 	import VolumeControl from '~/components/VolumeControl.vue'
-	import CountDown from '~/components/CountDown.vue'
 
-	const sounds = reactive([
-		{name: 'rain', src: '/sounds/rain.wav', dom: null, isPlaying: false},
-		{name: 'chwe', src: '/sounds/chew.wav', dom: null, isPlaying: false},
-		{name: 'mew', src: '/sounds/mew.wav', dom: null, isPlaying: false},
-		{name: 'thunder', src: '/sounds/thunder.wav', dom: null, isPlaying: false},
-		{name: 'test', src: '/sounds/test.wav', dom: null, isPlaying: false},
-	])
+	const soundStore = useSoundStore()
 
 	const rainAudioRef = ref(null)
 	const chweAudioRef = ref(null)
@@ -17,92 +11,62 @@
 	const thunderAudioRef = ref(null)
 	const testAudioRef = ref(null)
 
-	const masterVolume = ref(0.8)
-	const countDownRef = ref(null)
-
 	onMounted(() => {
-		sounds.find((sound) => sound.name === 'rain').dom = rainAudioRef.value
-		sounds.find((sound) => sound.name === 'chwe').dom = chweAudioRef.value
-		sounds.find((sound) => sound.name === 'mew').dom = mewAudioRef.value
-		sounds.find((sound) => sound.name === 'thunder').dom = thunderAudioRef.value
-		sounds.find((sound) => sound.name === 'test').dom = testAudioRef.value
-
-		sounds.forEach((sound) => {
-			if (sound.dom) {
-				sound.dom.volume = masterVolume.value
-			}
-		})
+		soundStore.setDom('rain', rainAudioRef.value)
+		soundStore.setDom('chwe', chweAudioRef.value)
+		soundStore.setDom('mew', mewAudioRef.value)
+		soundStore.setDom('thunder', thunderAudioRef.value)
+		soundStore.setDom('test', testAudioRef.value)
+		soundStore.setVolume(soundStore.masterVolume)
 	})
-
-	// 监听音量变化
-	watch(masterVolume, (val) => {
-		sounds.forEach((sound) => {
-			if (sound.dom) sound.dom.volume = val
-		})
-	})
-
-	const togglePlay = (currentSound) => {
-		if (!currentSound.dom) {
-			console.error(`Audio reference for '${currentSound.name}' is not set`)
-			return
-		}
-		const audioRef = currentSound.dom
-
-		if (audioRef.paused) {
-			sounds.forEach((sound) => {
-				if (sound !== currentSound && sound.dom) {
-					sound.dom.pause()
-					sound.dom.currentTime = 0
-					sound.isPlaying = false
-				}
-			})
-			audioRef.currentTime = 0
-			audioRef
-				.play()
-				.then(() => {
-					currentSound.isPlaying = true
-				})
-				.catch((e) => {
-					console.error(`Error playing ${currentSound.name} audio:`, e)
-					currentSound.isPlaying = false
-				})
-		} else {
-			audioRef.pause()
-			audioRef.currentTime = 0
-			currentSound.isPlaying = false
-		}
-	}
-
-	// 停止全部播放
-	function stopAll() {
-		sounds.forEach((sound) => {
-			if (sound.dom) {
-				sound.dom.pause()
-				sound.dom.currentTime = 0
-			}
-			sound.isPlaying = false
-		})
-	}
 </script>
 
 <template>
 	<div>
 		<div class="flex flex-row justify-center">
-			<audio ref="rainAudioRef" :src="sounds[0].src" preload="auto" loop />
-			<audio ref="chweAudioRef" :src="sounds[1].src" preload="auto" loop />
-			<audio ref="mewAudioRef" :src="sounds[2].src" preload="auto" loop />
-			<audio ref="thunderAudioRef" :src="sounds[3].src" preload="auto" loop />
-			<audio ref="testAudioRef" :src="sounds[4].src" preload="auto" loop />
+			<audio
+				ref="rainAudioRef"
+				:src="soundStore.sounds[0].src"
+				preload="auto"
+				loop
+			/>
+			<audio
+				ref="chweAudioRef"
+				:src="soundStore.sounds[1].src"
+				preload="auto"
+				loop
+			/>
+			<audio
+				ref="mewAudioRef"
+				:src="soundStore.sounds[2].src"
+				preload="auto"
+				loop
+			/>
+			<audio
+				ref="thunderAudioRef"
+				:src="soundStore.sounds[3].src"
+				preload="auto"
+				loop
+			/>
+			<audio
+				ref="testAudioRef"
+				:src="soundStore.sounds[4].src"
+				preload="auto"
+				loop
+			/>
 			<SoundCard
-				v-for="sound in sounds"
+				v-for="sound in soundStore.sounds"
 				:key="sound.name"
 				:sound="sound"
-				@togglePlay="togglePlay"
+				@togglePlay="soundStore.togglePlay"
 				class="mx-2"
 			/>
 		</div>
 		<div class="flex justify-center mt-8">
-			<VolumeControl v-model:masterVolume="masterVolume" />
+			<VolumeControl
+				v-model:masterVolume="soundStore.masterVolume"
+				@update:masterVolume="soundStore.setVolume"
+			/>
 		</div>
 	</div>
 </template>
---
+++ stores/global.ts
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia'
+import { ref, reactive } from 'vue'
+
+export const useSoundStore = defineStore('sound', () => {
+  const sounds = reactive([
+    { name: 'rain', src: '/sounds/rain.wav', dom: null, isPlaying: false },
+    { name: 'chwe', src: '/sounds/chew.wav', dom: null, isPlaying: false },
+    { name: 'mew', src: '/sounds/mew.wav', dom: null, isPlaying: false },
+    { name: 'thunder', src: '/sounds/thunder.wav', dom: null, isPlaying: false },
+    { name: 'test', src: '/sounds/test.wav', dom: null, isPlaying: false }
+  ])
+  const masterVolume = ref(0.8)
+
+  function setDom(name, dom) {
+    const sound = sounds.find(s => s.name === name)
+    if (sound) sound.dom = dom
+  }
+
+  function togglePlay(currentSound) {
+    if (!currentSound.dom) return
+    const audioRef = currentSound.dom
+    if (audioRef.paused) {
+      sounds.forEach(sound => {
+        if (sound !== currentSound && sound.dom) {
+          sound.dom.pause()
+          sound.dom.currentTime = 0
+          sound.isPlaying = false
+        }
+      })
+      audioRef.currentTime = 0
+      audioRef.play().then(() => {
+        currentSound.isPlaying = true
+      }).catch(() => {
+        currentSound.isPlaying = false
+      })
+    } else {
+      audioRef.pause()
+      audioRef.currentTime = 0
+      currentSound.isPlaying = false
+    }
+  }
+
+  function stopAll() {
+    sounds.forEach(sound => {
+      if (sound.dom) {
+        sound.dom.pause()
+        sound.dom.currentTime = 0
+      }
+      sound.isPlaying = false
+    })
+  }
+
+  function setVolume(val) {
+    masterVolume.value = val
+    sounds.forEach(sound => {
+      if (sound.dom) sound.dom.volume = val
+    })
+  }
+
+  return { sounds, masterVolume, setDom, togglePlay, stopAll, setVolume }
+})
