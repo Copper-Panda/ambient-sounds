dcbedfffb18a41699781cf09370348de6eab1f01
hticy <hticy3214@gmail.com>
2025-07-24 16:38:52 +0000

    new meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'AmbientSounds',
+      title: 'Ambient Sounds – Relax, Focus, and Sleep Better',
       meta: [
-        { name: 'description', content: 'Ambient is a modern, open-source, and privacy-focused personal dashboard.' }
+        { name: 'description', content: 'Immerse yourself in a world of calming audio designed to help you relax, concentrate, or drift into deep sleep. Our curated collection of ambient soundscapes—ranging from gentle rain, ocean waves, and forest birdsong to cozy fireplace crackles and soft white noise—creates the perfect atmosphere for any mood or activity.Whether you need a peaceful background while working, soothing sounds for meditation, or a calming playlist to unwind at night, Ambient Sounds offers a distraction-free, minimalist interface for an effortless listening experience.' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
