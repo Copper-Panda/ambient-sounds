f91e29e877a98ae03ea418a7b78f20dab232c2ea
hticy <hticy3214@gmail.com>
2025-07-25 01:12:47 +0000

    add bing meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
     head: {
       title: 'Ambient Sounds – Relax, Focus, and Sleep Better',
       meta: [
-        { name: 'description', content: 'Immerse yourself in a world of calming audio designed to help you relax, concentrate, or drift into deep sleep. Our curated collection of ambient soundscapes—ranging from gentle rain, ocean waves, and forest birdsong to cozy fireplace crackles and soft white noise—creates the perfect atmosphere for any mood or activity.Whether you need a peaceful background while working, soothing sounds for meditation, or a calming playlist to unwind at night, Ambient Sounds offers a distraction-free, minimalist interface for an effortless listening experience.' }
+        { name: 'description', content: 'Immerse yourself in a world of calming audio designed to help you relax, concentrate, or drift into deep sleep. Our curated collection of ambient soundscapes—ranging from gentle rain, ocean waves, and forest birdsong to cozy fireplace crackles and soft white noise—creates the perfect atmosphere for any mood or activity.Whether you need a peaceful background while working, soothing sounds for meditation, or a calming playlist to unwind at night, Ambient Sounds offers a distraction-free, minimalist interface for an effortless listening experience.' },
+        { name: 'msvalidate.01', content: '117B61D4E7E7A87A13B92E63097EFA1F'}
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
