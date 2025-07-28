06fb38011183e3640e1c877b7864b6d7096cb951
hticy <hticy3214@gmail.com>
2025-07-28 10:32:10 +0000

    add seo content at footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
   <footer class="px-10 py-4 flex text-sm text-[#AAB1B8] font-mono">
-    © made by hticy 2025
+    <section>
+      <h1 class="font-bold mb-2">Welcome to Ambient Sounds © made by hticy 2025</h1>
+      <p>Ambient Sounds helps you relax, focus, and sleep better with natural soundscapes like ocean waves, rain, forest birds, and white noise. 
+    Create your perfect audio atmosphere for work, meditation, or rest – no login or ads, just peaceful sounds anytime you need them.</p>
+    </section>
   </footer>
 </template>
